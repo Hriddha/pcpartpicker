b9acc02178e05d783974a9574dc47e959c58c609
Hriddha <praimsaha@gmail.com>
2026-04-23 04:46:41 +0600

    s

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-# Replace $PORT placeholder in nginx config
-sed -i "s/\$PORT/${PORT:-3000}/" /etc/nginx/http.d/default.conf
-
-# Start php-fpm in background
+sed -i "s/__PORT__/${PORT:-3000}/" /etc/nginx/http.d/default.conf
 php-fpm -D
-
-# Start nginx in foreground
 nginx -g 'daemon off;'
