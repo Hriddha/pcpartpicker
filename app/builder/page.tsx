f1a28d590ba4a209902714565adf13b2c870a4ff
Hriddha <praimsaha@gmail.com>
2026-04-25 18:36:24 +0600

    updated with suspense pc-builder.tsx

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -1,11 +1,13 @@
+import { Suspense } from "react";
 import { DashboardLayout } from "@/components/dashboard-layout";
 import { PCBuilder } from "@/components/pc-builder";
-export const dynamic = 'force-dynamic'
 
 export default function BuilderPage() {
   return (
     <DashboardLayout>
-      <PCBuilder />
+      <Suspense fallback={<div>Loading builder...</div>}>
+        <PCBuilder />
+      </Suspense>
     </DashboardLayout>
   );
 }
