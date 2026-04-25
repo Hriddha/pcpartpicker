3d7c7893fd33d499f6dfef0803d3e06a6ac7281f
Hriddha <praimsaha@gmail.com>
2026-04-25 18:57:46 +0600

    fixing pre-rendering crash

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from "react";
 import { DashboardLayout } from "@/components/dashboard-layout";
 import { PCBuilder } from "@/components/pc-builder";
 
+export const dynamic = "force-dynamic";
+
 export default function BuilderPage() {
   return (
     <DashboardLayout>
